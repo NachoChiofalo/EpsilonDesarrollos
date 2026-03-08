fd1223d556a2972449ca1c07c24601e56c6bb6e0
nacho <nachochiofalo@gmail.com>
2026-03-08 19:15:26 -0300

    [FIX] Solucion imagenes proyectos 5

--- src/data/projects.js
+++ src/data/projects.js
@@ -14,7 +14,7 @@ export const projects = [
     gradient: 'linear-gradient(135deg, #1e3a5f 0%, #0a1628 50%, #0d2137 100%)',
     category: 'GovTech',
     link: '#',
-    image: 'images/image.jpeg'
+    image: 'images/electoral.png'
   },
   {
     id: 2,
